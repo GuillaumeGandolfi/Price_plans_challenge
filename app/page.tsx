486289a109e6995f8b62114bddd75afdcae657e0
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2025-03-11 17:10:00 +0100

    Background with ellipses and blur effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
 export default function Page() {
-  return <section></section>;
+  return (
+    <section className="relative min-h-screen bg-[#010B25] overflow-hidden">
+      {/* 2 ellipses pour ajouter un effet au background */}
+      <div
+        className="absolute z-0 bg-[#1241C5]/50 rounded-full blur-[80px]"
+        style={{
+          width: "50px",
+          height: "1200px",
+          bottom: 0,
+          left: "50%",
+          transform: "translateX(-100%) rotate(-45deg)",
+          transformOrigin: "bottom center",
+        }}
+      />
+
+      <div
+        className="absolute z-0 bg-[#1241C5]/50 rounded-full blur-[80px]"
+        style={{
+          width: "50px",
+          height: "1200px",
+          bottom: 0,
+          right: 0,
+          transform: "translateX(-80%) rotate(-45deg)",
+          transformOrigin: "bottom right",
+        }}
+      />
+    </section>
+  );
 }
